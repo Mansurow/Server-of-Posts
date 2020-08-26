c63bb870321ca9137ca5d08dd98527ef3c38d06b
Vlad <vladik.mansurov2002@mail.ru>
2020-08-26 17:36:18 +0500

    refactor: деструктуризация в аргументах обработчиков

--- main.js
+++ main.js
@@ -10,15 +10,12 @@ let nextId = 1;
 const posts = [];
 
 const methods = new Map();
-methods.set('/posts.get',function(request,response){
+methods.set('/posts.get',function({response}){
     response.writeHead(statusOk, {'Content-Type': 'application/json'});
     response.end(JSON.stringify(posts));
 });
-methods.set('/posts.getById',function(request,response){});
-methods.set('/posts.post',function(request,response){
-    const url = new URL(request.url, `http://${request.headers.host}`);
-    const searchParams = url.searchParams;
-
+methods.set('/posts.getById',function(){});
+methods.set('/posts.post',function({response, searchParams}){
     if(!searchParams.has('content')){
         response.writeHead(statusBadRequest);
         response.end();
@@ -35,13 +32,11 @@ methods.set('/posts.post',function(request,response){
     response.writeHead(statusOk,{'Content-Type': 'application/json'});
     response.end(JSON.stringify(post));
 });
-methods.set('/posts.edit',function(request,response){});
-methods.set('/posts.delete',function(request,response){});
+methods.set('/posts.edit',function(){});
+methods.set('/posts.delete',function(){});
 
 const server = http.createServer(function(request, response){
-    const url = new URL(request.url, `http://${request.headers.host}`);
-    const pathname = url.pathname;
-    const searchParams = url.searchParams;
+    const {pathname, searchParams} = new URL(request.url, `http://${request.headers.host}`);
 
     const method = methods.get(pathname);
     if (method === undefined){
@@ -50,7 +45,14 @@ const server = http.createServer(function(request, response){
         return;
     }
 
-    method(request,response);
+    const params = {
+        request,
+        response,
+        pathname,
+        searchParams,
+    }
+
+    method(params);
 });
 
 const port = 9999;
